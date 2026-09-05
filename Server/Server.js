44e677ee53002354415a68f1755f42ec3aae8923
MyCreations-R <rehan.p.work@gmail.com>
2026-09-06 01:47:11 +0530

    Prepare app for deployment

--- Server/Server.js
+++ Server/Server.js
@@ -21,7 +21,7 @@ dotenv.config();
 // Create Express app
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // ===============================
 // MIDDLEWARE
@@ -453,6 +453,6 @@ app.delete("/api/chats/:chatId/messages", (req, res) => {
 // START SERVER
 // ===============================
 
-app.listen(PORT, () => {
-  console.log(`Backend running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on port ${PORT}`);
 });
